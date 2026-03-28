c7f636944118e4e1cc1d825fb985f391381d20b0
Juan Barraza <juanbarraza2004@outlook.com>
2026-03-28 13:22:43 -0500

    feat: added eventDomain

---
+++ crud-users/Domain/Events/EventDomain.php
@@ -0,0 +1,39 @@
+<?php
+
+
+abstract class EventDomain
+{
+    private $eventName;
+    private $occurredOn;
+
+    public function __construct($eventName)
+    {
+        $this->eventName = $eventName;
+        $this->occurredOn = date('Y-m-d H:i:s');
+    }
+
+    public function eventName()
+    {
+        return $this->eventName;
+    }
+
+    public function occurredOn()
+    {
+        return $this->occurredOn;
+    }
+
+    abstract public function payload();
+
+}
+
+
+
+
+
+
+
+
+
+
+
+?>
